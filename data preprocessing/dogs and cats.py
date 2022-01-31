b51888a0db765f65413153b1209a1fcf48fcc363
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2022-01-31 11:29:02 +0530

    Update and rename dogs and cats to dogs and cats.py

--- data preprocessing/dogs and cats.py
+++ data preprocessing/dogs and cats.py
@@ -1,9 +1,12 @@
 dir=""
 categories=['dogs','cats']
+dat[]
 for category in categories : 
     folder=os.path.join(dir,category)
+    label=categories.index(category)
     for image in os.listdir(folder): 
         image_path=os.path.join(folder,image)
         image_=cv2.imread(image_path)
         image__=cv2.resize(image,(220,220))
+        data.append([image__,label])
         plt.show(image__)
